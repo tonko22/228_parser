093ae0454c6266b357b6df8a0e84daaad4882f98
tonko22 <tonko22x@gmail.com>
2019-02-02 14:46:38 +0300

    Added argparse, merge extractor to single func, added exceptions

--- export.py
+++ export.py
@@ -1,13 +1,15 @@
 import csv
-from typing import List
+from typing import Dict
+import os.path
 
-def write_csv(parsed_doc_dicts: List, filename="result.csv"):
+def write_csv(result: Dict, filename="result.csv"):
     """ If csvfile is a file object, it should be opened with newline=''
     """
-    with open(filename, 'w', newline='') as csvfile:
-        fieldnames = ["Суд", "Дата приговора", "ФИО"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for parsed_dict in parsed_doc_dicts:
-            writer.writerow(parsed_dict)
+    with open(filename, 'a', newline='') as csvfile:
+        if not os.path.isfile(fname):
+            fieldnames = ["Суд", "Дата приговора", "ФИО"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            
+        writer.writerow(parsed_dict)
         
