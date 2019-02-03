325124a839de7bda65023705062d7c26c074ec01
tonko22 <tonko22x@gmail.com>
2019-02-03 21:36:14 +0300

    Refacroing, bugifxes

--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@ import os
 import docx
 import logging 
 from ner import EntityExtractor
+
 import csv
 from typing import Dict
 
@@ -11,12 +12,12 @@ logger = logging.getLogger()
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(levelname)s %(funcName)s %(message)s')
 
 class ParsingHandler():
+    log_path = "log.csv"
     def __init__(self, args):
         self.skips_n_reasons = {}
         self.args = args
         
-        self.succ_counter = 0
-        self.error_counter = 0
+        self.skipped_files = 0
         
         self.process_files()
         self.report()
@@ -37,12 +38,12 @@ class ParsingHandler():
         logger.critical(msg)
         self.skips_n_reasons.update({filename: msg})
                 
-    def write_csv(self, result: Dict):
+    def write_result_csv(self, result: Dict):
         """ If csvfile is a file object, it should be opened with newline=''
         """
         file_exists = os.path.isfile(self.args.csv_path)        
         with open(self.args.csv_path, 'a', newline='') as csvfile:
-            fieldnames = ['Суд', 'Дата приговора', 'ФИО', 'Смягчающие обстоятельства', 'Вид наказания', 'Особый порядок',
+            fieldnames = ["Ссылка", "Файл", 'Суд', 'Дата приговора', 'ФИО', 'Смягчающие обстоятельства', 'Вид наказания', 'Особый порядок',
                           'Отбывал ли ранее лишение свободы', 'Судимость', 'Наркотики', 'Срок наказания в месяцах',
                           'Отягчающие обстоятельства']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -51,9 +52,9 @@ class ParsingHandler():
             writer.writerow(result)
         
     def write_error_log(self, error_dict):
-        file_exists = os.path.isfile(self.args.log_path)        
-        with open(self.args.log_path, 'a', newline='') as csvfile:
-            fieldnames = ['filename', "error"]
+        file_exists = os.path.isfile(self.log_path)        
+        with open(self.log_path, 'a', newline='') as csvfile:
+            fieldnames = ['filename', "extractor_errors", "ner_errors"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             if not file_exists:
                 writer.writeheader()
@@ -61,37 +62,41 @@ class ParsingHandler():
             
     def process_files(self):
         filenames = os.listdir(self.args.target_dir)
-        total = len(filenames)
+        self.total_files = len(filenames)
         for n, filename in enumerate(filenames):
-            logger.info("Processing {}, ({}/{})".format(filename, n, total))
+            logger.info("Processing {}, ({}/{})".format(filename, n, self.total_files))
+            
+            error_dict = {"filename": filename,
+            "extractor_errors": None,
+            "ner_errors": None}
+            
             try:
                 text = self.extract_text(filename)
-                ex = EntityExtractor(text)
-                if ex.errors:
-                    self.write_error_log({filename: ex.errors})
-                self.write_csv(ex.summary_dict)
-                self.succ_counter += 1
             except BaseException as e:
-                self.error_counter += 1
-                error_dict = {
-                    "filename": filename,
-                    "error": str(e)+str(e.args)
-                }
-                logger.error("Error while processing {}".format(error_dict))
-                if self.args.log_path:
-                    self.write_error_log(error_dict)
+                logger.error("Error while extracting {}, skipping".format(e))
+                error_dict["extractor_errors"] = e
+                self.skipped_files += 1
+                continue
+                    
+            ex = EntityExtractor(filename, text)
+            if ex.errors:
+                error_dict["ner_errors"] = e
                 
-     
+            self.write_result_csv(ex.summary_dict)
+            
+            if self.args.write_logs:
+                    self.write_error_log(error_dict)
+                    
     def report(self):
         logger.info(
-            "Files processed: {}, errors(skipped): {}".format(
-                self.succ_counter, self.error_counter))
+            "Files processed: {}, skipped: {}".format(
+                self.total_files, self.error_counter))
     
     
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Извлечение атрибутов приговоров из указанной папки в .csv файл. Допустимые форматы: .txt, .doc, docx')
     parser.add_argument('--target_dir', default="test_txt")
     parser.add_argument('--csv_path', default="result.csv")
-    parser.add_argument('--log_path', default="") # acts as false with if
+    parser.add_argument('--write_logs', action="store_false")
     args = parser.parse_args()
     ParsingHandler(args)
