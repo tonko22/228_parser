eafa9970e5b2b2066a4355813c92e8408279ea56
tonko22 <tonko22x@gmail.com>
2019-03-05 17:45:32 +0300

    Updated analysis with date parsing fix

--- parse.py
+++ parse.py
@@ -4,7 +4,7 @@ import asyncio
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Извлечение атрибутов приговоров из указанной папки в .csv файл. Допустимые форматы: .txt, .doc, docx')
-    parser.add_argument('--target_dir', default="test_txt")
+    parser.add_argument('--target_dir', default="data/part2")
     parser.add_argument('--result_path', default="result.csv")
     parser.add_argument('--error_log_path', default="error_log.csv")
     parser.add_argument('--async', action="store_true") # default false
